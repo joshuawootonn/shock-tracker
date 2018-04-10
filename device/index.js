4c75bee60cc3ec0882b07d052b1262ca7c9f3f74
Ted Paulsen <ted.m.paulsen@gmail.com>
2018-04-10 02:56:19 +0000

    Moved device code into git repo

---
+++ device/index.js
@@ -0,0 +1,36 @@
+var bleno = require('bleno');
+var BlenoPrimaryService = bleno.PrimaryService;
+var EchoCharacteristic = require('./characteristics/echo-characteristic');
+
+var devicename = 'shockIOT';
+var deviceuuid = ['B8:27:EB:27:65:9B'];
+
+bleno.on('stateChange', function(state) {
+	console.log('on -> stateChange: ' + state);
+
+	if (state == 'poweredOn') {
+		bleno.startAdvertising(devicename, deviceuuid);
+	} else {
+		bleno.stopAdvertising();
+	}
+});
+
+bleno.on('advertisingStart', function(error) {
+	console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+
+	if (!error) {
+		bleno.setServices([
+			new BlenoPrimaryService({
+				uuid: 'B8:27:EB:27:65:9B',
+				characteristics: [
+					new EchoCharacteristic()
+				]
+			})
+		]);
+	}
+});
+
+bleno.on('accept', function(clientAddr) {
+    console.log('on -> accept client address: ' + clientAddr);
+    
+});
