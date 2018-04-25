bdefacf8c94fd52a96295b33308e468904264a78
Joshua Wootonn <joshuawootonn@gmail.com>
2018-04-25 14:09:15 -0500

    Started the project, found and organized dependencies, moving to main computer

---
+++ web-client/web-client-react/src/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './styles/styles.css';
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">Hello fasdfas</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
